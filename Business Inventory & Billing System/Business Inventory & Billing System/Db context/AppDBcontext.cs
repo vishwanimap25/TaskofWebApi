bb9390495dc32bfe3e8e0c5f488eeabe7bfe77e3
vishwanimap25 <Vishwajeet.awale@nimapinfotech.com>
2025-07-03 18:23:09 +0530

    mvc practiced

---
+++ Business Inventory & Billing System/Business Inventory & Billing System/Db context/AppDBcontext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Business_Inventory___Billing_System.Db_context
+{
+    public class AppDBcontext : DbContext
+    {
+        public AppDBcontext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected AppDBcontext()
+        {
+        }
+    }
+}
