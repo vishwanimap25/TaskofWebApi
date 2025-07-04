935bacb19573c36815bbabe831109c9a058cde4e
vishwanimap25 <Vishwajeet.awale@nimapinfotech.com>
2025-07-04 18:18:08 +0530

    mvc

---
+++ WebApplication1/WebApplication1/App DB/ApplicationDBcontext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.AppDB
+{
+    public class ApplicationDBcontext : DbContext
+    {
+        public ApplicationDBcontext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected ApplicationDBcontext()
+        {
+        }
+
+        //public DbSet<User> User { get; set; }
+
+        public DbSet<User> User { get; set; }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column("StudentName", TypeName = "varchar(100)")]
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
