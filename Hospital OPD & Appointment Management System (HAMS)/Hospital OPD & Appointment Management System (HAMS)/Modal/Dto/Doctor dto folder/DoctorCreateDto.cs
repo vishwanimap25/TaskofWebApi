10486cb6f2b77f8ebff4630152eb06c2ceda5614
vishwanimap25 <Vishwajeet.awale@nimapinfotech.com>
2025-07-10 18:28:37 +0530

    half complete need to debug/ helped by akshay

--- Hospital OPD & Appointment Management System (HAMS)/Hospital OPD & Appointment Management System (HAMS)/Modal/Dto/Doctor dto folder/DoctorCreateDto.cs
+++ Hospital OPD & Appointment Management System (HAMS)/Hospital OPD & Appointment Management System (HAMS)/Modal/Dto/Doctor dto folder/DoctorCreateDto.cs
@@ -4,25 +4,17 @@ namespace Hospital_OPD___Appointment_Management_System__HAMS_.Modal.Dto.Doctor_d
 {
     public class DoctorCreateDto
     {
-        [Required]
         public string FullName { get; set; }
-        [Required]
         public string Specialization { get; set; }
-        [Required]
         public string Email { get; set; }
-        [Required]
         
         public string PhoneNumber { get; set; }
         public string Qualification { get; set; }
-        [Required]
 
         public string Gender { get; set; }
         public string AvailbilityDays { get; set; }
 
-
-        [Required]
         public TimeSpan AvailableFrom { get; set; }  
-        [Required]
         public TimeSpan AvailableTo { get; set; }
         public bool IsActive { get; set; }
 
